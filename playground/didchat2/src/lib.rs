561a62e907a283f6241f5d76e3d19e95ecd61d2b
Jonas Johan Solsvik <jonasjso@protonmail.com>
2021-03-03 19:37:55 +0100

    Returning output from each cmd function

--- playground/didchat2/src/lib.rs
+++ playground/didchat2/src/lib.rs
@@ -1,13 +1,9 @@
-//
-// Instrument with 'EnumFromStr' trait, to be able to parse enum from string.
-//
 // @see https://stackoverflow.com/a/39070533
 #[macro_use]
 extern crate custom_derive;
 #[macro_use]
 extern crate enum_derive;
 custom_derive! {
-    // format!("{}") uses 'Display', format!("{:?}") uses 'Debug'
     #[derive(Debug, EnumFromStr)]
     enum CMD {
         Login,
@@ -25,7 +21,6 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, std::io::Error> {
-
         let valid_cmds = vec!["help", "login", "connect", "send", "read"];
         let default_cmd = String::from("help");
         let cmd = args.get(1).unwrap_or(&default_cmd);
@@ -46,46 +41,42 @@ impl Config {
 
 
 pub fn run(config: Config) -> Result<(), std::io::Error> {
-    match config.cmd {
+    let output = match config.cmd {
         CMD::Login => login(),
         CMD::Connect => connect(),
         CMD::Send => send(),
         CMD::Read => read(),
         CMD::Help => help()
-    }
-}
+    }?;
 
-fn login() -> Result<(), std::io::Error> {
-    println!("Login to user");
+    println!("{}", output);
     Ok(())
 }
 
-fn connect() -> Result<(), std::io::Error> {
-    println!("Connecting to friend");
-    Ok(())
+fn login() -> Result<String, std::io::Error> {
+    Ok(String::from("Login to user"))
 }
 
-fn send() -> Result<(), std::io::Error> {
-    println!("Sending message");
-    Ok(())
+fn connect() -> Result<String, std::io::Error> {
+    Ok(String::from("Connecting to friend"))
 }
 
-fn read() -> Result<(), std::io::Error> {
-    println!("Reading message");
-    Ok(())
+fn send() -> Result<String, std::io::Error> {
+    Ok(String::from("Sending message"))
 }
 
-fn help() -> Result<(), std::io::Error> {
-    println!("
+fn read() -> Result<String, std::io::Error> {
+    Ok(String::from("Reading message"))
+}
+
+fn help() -> Result<String, std::io::Error> {
+    Ok(String::from("
     Usage: 
 
         cargo run <login|connect|send|read|help>
         didchat   <login|connect|send|read|help>
-");
-    Ok(())
+"))
 }
-
-
 
 // @see https://stackoverflow.com/a/38406885
 fn uppercase_first_letter(s: &str) -> String {
