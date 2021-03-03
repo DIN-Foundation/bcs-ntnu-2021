01f418a21b97ce7ac8da32cf1c27cd5731a2cf16
Jonas Johan Solsvik <jonasjso@protonmail.com>
2021-03-03 19:48:41 +0100

    cherry-pick didchat changes into main

--- playground/didchat/src/main.rs
+++ playground/didchat/src/main.rs
@@ -1,17 +1,9 @@
-use std::env;
-use didcomm_rs::{
-    Message,
-};
-use didcomm_rs::crypto::{
-    CryptoAlgorithm
-};
-use x25519_dalek::{PublicKey, StaticSecret};
-use rand_core::OsRng;
+use std::io::Write;
 
 fn main() {
     let jonas = "did:xyz:ulapcuhsatnpuhza930hpu34n_";
     let cecilie = vec!("did::xyz:34r3cu403hnth03r49g03");
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
     let args_as_string = args.join("");
     let args_as_bytes = args_as_string.as_bytes();
 
@@ -19,7 +11,7 @@ fn main() {
 
     // 1. Unencrypted
     {
-        let message = Message::new()
+        let message = didcomm_rs::Message::new()
             .from(jonas)
             .to(cecilie.clone())
             .body(args_as_bytes);
@@ -30,25 +22,30 @@ fn main() {
             .unwrap();
 
         println!("---------UNECNRYPTED---------");
-        println!("{:?}", serialized_message);
+        println!("{}", serialized_message);
         println!("---------UNECNRYPTED---------");
     }
-
+    
     // 2. Encrypted
     {
-        let jonas_secret = StaticSecret::new(OsRng);
-        let cecilie_secret = StaticSecret::new(OsRng);
-        let _ = PublicKey::from(&jonas_secret);
-        let cecilie_public = PublicKey::from(&cecilie_secret);
+        let jonas_secret   = x25519_dalek::StaticSecret::new(rand_core::OsRng);
+        let cecilie_secret = x25519_dalek::StaticSecret::new(rand_core::OsRng);
+        let _jonas_public  = x25519_dalek::PublicKey::from(&jonas_secret);
+        let cecilie_public = x25519_dalek::PublicKey::from(&cecilie_secret);
 
+        std::fs::create_dir(".didchat").unwrap_or_default();
+
+        let mut _file = std::fs::File::create(".didchat/key.private").unwrap();
+        _file.write_all(&jonas_secret.to_bytes()).unwrap();
+        
         let shared_secret = jonas_secret.diffie_hellman(&cecilie_public);
 
-        let message = Message::new()
+        let message = didcomm_rs::Message::new()
             .from(jonas)
             .to(cecilie.clone())
             .timed(Some(3600))
             .body(args_as_bytes)
-            .as_jwe(&CryptoAlgorithm::XC20P);
+            .as_jwe(&didcomm_rs::crypto::CryptoAlgorithm::XC20P);
 
         let serialized_message = message
             .seal(shared_secret.as_bytes())
