48041c55c1641f14ab2534ad8a45ed456ef8cbc4
Jonas Johan Solsvik <jonasjso@protonmail.com>
2021-03-12 12:27:25 +0100

    implement didchat doc and dichat did commands

--- playground/didchat2/src/lib.rs
+++ playground/didchat2/src/lib.rs
@@ -1,20 +1,21 @@
 #[derive(Debug)]
 enum CMD {
+    Doc,
+    Did,
     Login,
-    Connect,
+    Friend,
     Send,
     Read,
     Help
 }
 
-
 pub struct Config {
-    cmd: CMD, // login | connect | send | read | help
+    cmd: CMD,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, std::io::Error> {
-        let valid_cmds = vec!["help", "login", "connect", "send", "read"];
+        let valid_cmds = vec!["help", "did", "doc", "login", "friend", "send", "read"];
         let default_cmd = String::from("help");
         let cmd = args.get(1).unwrap_or(&default_cmd);
 
@@ -25,8 +26,10 @@ impl Config {
         };
 
         let cmd: CMD = match &cmd[..] {
+            "did" => CMD::Did,
+            "doc" => CMD::Doc,
             "login" => CMD::Login,
-            "connect" => CMD::Connect,
+            "friend" => CMD::Friend,
             "send" => CMD::Send,
             "read" => CMD::Read,
             "help" => CMD::Help,
@@ -40,29 +43,21 @@ impl Config {
 pub fn run(config: Config) -> Result<String, std::io::Error> {
     match config.cmd {
         CMD::Login => login(),
-        CMD::Connect => connect(),
+        CMD::Doc => doc(),
+        CMD::Did => did(),
+        CMD::Friend => friend(),
         CMD::Send => send(),
         CMD::Read => read(),
         CMD::Help => help()
     }
-}
-use std::io::Write;
-
-/**
- * https://tools.ietf.org/html/draft-ietf-jose-cfrg-curves-06#section-2
- */
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-struct X25519JWK {
-    kty: String, // Must be "OKP"
-    crv: String, // Must be "Ed25519"
-    x: String,   // base64 encoded public key
-    d: String,   // base64 encoded private key
 }
 
 /**
  * Login - Creates a public/private key-pair if does not already exists, linked to the given name.
  */ 
 fn login() -> Result<String, std::io::Error> {
+    use std::io::Write;
+
     if !std::fs::metadata(".didchat/").is_ok() {
         std::fs::create_dir_all(".didchat/").unwrap_or_default();
     }
@@ -70,19 +65,56 @@ fn login() -> Result<String, std::io::Error> {
     let seed_path = format!(".didchat/seed");
 
     if !std::fs::metadata(&seed_path).is_ok() {
+        // 1. Generate seed
         let mut csprng = rand_core::OsRng{};
         let seed = ed25519_dalek::SecretKey::generate(&mut csprng);
         let seed_bytes = seed.as_bytes();
+
+        // 2. Write seed to file
         let mut file = std::fs::File::create(seed_path).unwrap();
         file.write(seed_bytes).unwrap();
+        
         Ok(format!("New user signed in."))
     } else {
         Ok(format!("Existing user already signed in."))
     }
 }
 
-fn connect() -> Result<String, std::io::Error> {
-    Ok(String::from("Connecting to friend"))
+fn doc() -> Result<String, std::io::Error> {
+    use did_key::DIDCore;
+    use did_key::KeyMaterial;
+    
+    // 1. Read seed from file
+    let seed = std::fs::read(".didchat/seed").unwrap();
+
+    // 2. Transform seed to a did-document
+    let public_private_keypair = did_key::Ed25519KeyPair::from_seed(&seed);
+    let public_only_keypair = did_key::from_existing_key::<did_key::Ed25519KeyPair>(&public_private_keypair.public_key_bytes(), None);
+    let doc = public_only_keypair.get_did_document(did_key::CONFIG_LD_PUBLIC);
+
+    // 3. Serialize did-document to json
+    let json = serde_json::to_string_pretty(&doc).unwrap();
+    Ok(format!("{}", json))
+}
+
+fn did() -> Result<String, std::io::Error> {
+    use did_key::DIDCore;
+
+    // 1. Read seed from file
+    let seed = std::fs::read(".didchat/seed").unwrap();
+
+    // 2. Transform seed to a did
+    let keypair = did_key::Ed25519KeyPair::from_seed(&seed);
+    let diddoc: did_key::Document = keypair.get_did_document(did_key::CONFIG_LD_PUBLIC);
+    let did = diddoc.id;
+
+    // 3. Print did
+    Ok(format!("{}", did))
+}
+
+
+fn friend() -> Result<String, std::io::Error> {
+    Ok(String::from("Added friend"))
 }
 
 fn send() -> Result<String, std::io::Error> {
@@ -97,8 +129,8 @@ fn help() -> Result<String, std::io::Error> {
     Ok(String::from("
     Usage: 
 
-        cargo run <login|connect|send|read|help>
-        didchat   <login|connect|send|read|help>
+        cargo run <login|doc|friend|send|read|help>
+        didchat   <login|doc|friend|send|read|help>
 
         didchat login <username>
 "))
