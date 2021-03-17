5d0af2bc93769b8446132f0ca95950383ace0cf6
Jonas Johan Solsvik <jonasjso@protonmail.com>
2021-03-17 12:24:07 +0100

    didchat / list messages in chronological order

--- playground/didchat2/src/lib.rs
+++ playground/didchat2/src/lib.rs
@@ -3,28 +3,30 @@ pub fn run(config: Config) -> Result<String, std::io::Error> {
         CMD::Init{ path } => init(&path),
         CMD::Doc{ path } => doc(&path),
         CMD::Did{ path } => did(&path),
+        CMD::Messages{ path } => messages(&path),
         CMD::Connect{ path, name, did } => connect(&path, &name, &did),
-        CMD::Send{ path, name, message } => send(&path, &name, &message),
-        CMD::Open{ path, encrypted_message } => open(&path, &encrypted_message),
+        CMD::Write{ path, name, message } => write(&path, &name, &message),
+        CMD::Read{ path, encrypted_message } => read(&path, &encrypted_message),
         CMD::Help => help()
     }
 }
+
 
 fn init(path: &str) -> Result<String, std::io::Error> {
     use std::io::Write;
 
     // 1. Create empty folders
     if !std::fs::metadata(root_path(path)).is_ok() {
-        std::fs::create_dir_all(root_path(path)).unwrap_or_default();
+        std::fs::create_dir_all(root_path(path))?;
     }
     if !std::fs::metadata(names_path(path)).is_ok() {
-        std::fs::create_dir_all(names_path(path)).unwrap_or_default();
+        std::fs::create_dir_all(names_path(path))?;
     }
     if !std::fs::metadata(dids_path(path)).is_ok() {
-        std::fs::create_dir_all(dids_path(path)).unwrap_or_default();
+        std::fs::create_dir_all(dids_path(path))?;
     }
     if !std::fs::metadata(messages_path(path)).is_ok() {
-        std::fs::create_dir_all(messages_path(path)).unwrap_or_default();
+        std::fs::create_dir_all(messages_path(path))?;
     }
 
     if !std::fs::metadata(seed_path(path)).is_ok() {
@@ -43,6 +45,7 @@ fn init(path: &str) -> Result<String, std::io::Error> {
     }
 }
 
+
 fn doc(path: &str) -> Result<String, std::io::Error> {
     use did_key::DIDCore;
     use did_key::KeyMaterial;
@@ -60,6 +63,7 @@ fn doc(path: &str) -> Result<String, std::io::Error> {
     Ok(format!("{}", did_document))
 }
 
+
 fn did(path: &str) -> Result<String, std::io::Error> {
     use did_key::DIDCore;
 
@@ -75,6 +79,7 @@ fn did(path: &str) -> Result<String, std::io::Error> {
     Ok(format!("{}", did))
 }
 
+
 fn connect(path: &str, name: &str, did: &str) -> Result<String, std::io::Error> {
     use std::io::Write;
 
@@ -89,110 +94,130 @@ fn connect(path: &str, name: &str, did: &str) -> Result<String, std::io::Error> 
     Ok(format!("{}\n{}", name_path(path, name), did_path(path, did)))
 }
 
-fn send(path: &str, name: &str, message: &str) -> Result<String, std::io::Error> {
-    use did_key::Ecdh;
+
+fn write(path: &str, name: &str, message: &str) -> Result<String, std::io::Error> {
     use did_key::KeyMaterial;
-    use did_key::DIDCore;
     use std::io::Write;
 
-    // 1. Read from and to x25519 keys from file
+    // 1. Read from-key
     let from_seed = std::fs::read(seed_path(path)).unwrap();
     let from_key = did_key::Ed25519KeyPair::from_seed(&from_seed);
-    let from_did = from_key.get_did_document(did_key::CONFIG_LD_PUBLIC).id;
-    let from_key = from_key.get_x25519();
     
+    // 2. Read to-key
     let to_did = std::fs::read_to_string(name_path(path, name)).unwrap();
     let to_key = did_key::resolve(&to_did).unwrap();
     let to_key = did_key::Ed25519KeyPair::from_public_key(&to_key.public_key_bytes());
-    let to_key = to_key.get_x25519();
 
-    // 2. Generate elliptic-curve-diffie-hellmann (ecdh) shared secret
-    let shared_secret = from_key.key_exchange(&to_key);
-
-    // 3. Construct didcomm message
-    let to_vec = vec![&to_did[..]];
-    
-    let message = didcomm_rs::Message::new()
-        .from(&from_did)
-        .to(&to_vec[..])
-        .timed(Some(3600))
-        .body(message.as_bytes())
-        .as_jwe(&didcomm_rs::crypto::CryptoAlgorithm::XC20P);
-
-    // 4. Seal/encrypt message using shared secret
-    let encrypted_message = message
-        .seal(&shared_secret)
-        .unwrap();
-
-    // 5. Log encrypted message to file, to keep the message history
+    // 3. Encrypt message with from_key, to keep message history in local file
+    let encrypted_message = encrypt_didcomm(&from_key, &from_key, message).unwrap();
     let mut file = std::fs::File::create(message_path(path)).unwrap();
     file.write(encrypted_message.as_bytes()).unwrap();
+
+    // 4. Encrypt message with to_key, to prepare it for transmission
+    let encrypted_message = encrypt_didcomm(&from_key, &to_key, message).unwrap();
     
     Ok(format!("{}", &encrypted_message))
 }
 
-fn open(path: &str, encrypted_message: &str) -> Result<String, std::io::Error> {
-    use did_key::Ecdh;
-    use did_key::KeyMaterial;
+
+fn read(path: &str, encrypted_message: &str) -> Result<String, std::io::Error> {
     use std::io::Write;
+    use did_key::KeyMaterial;
 
-    // 1. Read "to"-key from file
-    let to_seed = std::fs::read(seed_path(path)).unwrap();
-    let to_key = did_key::Ed25519KeyPair::from_seed(&to_seed);
-    let to_key = to_key.get_x25519();
-
-    // 2. Read "from"-key from "dcem"-header.
-    let jwe: didcomm_rs::Jwe = serde_json::from_str(encrypted_message).unwrap();
-    let from_did = jwe.from().as_ref().unwrap();
-    let from_key = did_key::resolve(&from_did).unwrap();
-    let from_key = did_key::Ed25519KeyPair::from_public_key(&from_key.public_key_bytes());
-    let from_key = from_key.get_x25519();
-
-    // 3. Generate elliptic-curve-diffie-hellmann (ecdh) shared secret
-    let shared_secret = to_key.key_exchange(&from_key);
-
-    // 4. Decrypt message
-    let opened = didcomm_rs::Message::receive(encrypted_message, Some(&shared_secret), None);
-    let opened = opened.unwrap(); // @unwrap!
-    let opened = String::from_utf8(opened.body).unwrap(); // @unwrap!
-
-    // 5. Map did to name, if exists
-    let from_name = std::fs::read_to_string(did_path(path, from_did))
-        .unwrap_or(from_did.clone());
-
-    // 6. Log encrypted message to file, to keep the message history
-    let mut file = std::fs::File::create(message_path(path)).unwrap();
+    // 1. Store incomming message to file, to keep the message history
+    let message_fpath = message_path(path);
+    let message_fpath = std::path::Path::new(&message_fpath);
+    let mut file = std::fs::File::create(message_fpath).unwrap();
     file.write(encrypted_message.as_bytes()).unwrap();
 
-    Ok(format!("{} > {}", from_name, opened))
+    // 2. Get to-key
+    let to_seed = std::fs::read(seed_path(path)).unwrap();
+    let to_key = did_key::Ed25519KeyPair::from_seed(&to_seed);
+    
+    // 3. Get from-key
+    let from_jwe: didcomm_rs::Jwe = serde_json::from_str(&encrypted_message).unwrap();
+    let from_did = from_jwe.from().as_ref().unwrap();
+    let from_key = did_key::resolve(&from_did).unwrap();
+    let from_key = did_key::Ed25519KeyPair::from_public_key(&from_key.public_key_bytes());
+
+    // 4. Decrypt message
+    let decrypted = decrypt_didcomm(&from_key, &to_key, encrypted_message).unwrap();
+
+    // 5. Format
+    let from_name = std::fs::read_to_string(did_path(path, from_did))
+        .unwrap_or(from_did.clone());
+    let filename = &message_fpath.file_name().unwrap().to_str().unwrap();
+    Ok(format!("[{}] {} > {}", filename, from_name, decrypted))
 }
+
+
+fn messages(path: &str) -> Result<String, std::io::Error> {
+    use did_key::KeyMaterial;
+
+    let mut result = String::from("");
+
+    let mut entries: Vec<std::fs::DirEntry> = std::fs::read_dir(messages_path(path)).unwrap().filter_map(|f| f.ok()).collect();
+    entries.sort_by_key(|e| e.path());
+
+    for entry in entries {
+        if entry.path().is_dir() {
+            continue;
+        }
+        let encrypted_message = std::fs::read_to_string(entry.path())?;
+
+        // 1. Get to-key
+        let to_seed = std::fs::read(seed_path(path)).unwrap();
+        let to_key = did_key::Ed25519KeyPair::from_seed(&to_seed);
+
+        // 2. Get from-key
+        let from_jwe: didcomm_rs::Jwe = serde_json::from_str(&encrypted_message).unwrap();
+        let from_did = from_jwe.from().as_ref().unwrap();
+        let from_key = did_key::resolve(&from_did).unwrap();
+        let from_key = did_key::Ed25519KeyPair::from_public_key(&from_key.public_key_bytes());
+
+        // 3. Decrypt message
+        let decrypted = decrypt_didcomm(&from_key, &to_key, &encrypted_message).unwrap();
+
+        // 4. Format
+        let from_name = std::fs::read_to_string(did_path(path, from_did))
+            .unwrap_or(from_did.clone());
+        let file_name = String::from(entry.file_name().to_str().unwrap());
+        result.push_str(&format!("[{}] {} > {}\n", file_name, from_name, decrypted));
+    }
+
+    Ok(result)
+}
+
 
 fn help() -> Result<String, std::io::Error> {
     Ok(String::from("
     Usage:
+        didchat <path> <command>
+        
         didchat <path> init
         didchat <path> doc    
         didchat <path> did    
+        didchat <path> messages
 
         didchat <path> connect <name> <did>
 
-        didchat <path> send <name> <message>      -->  <encrypted message>
-        didchat <path> open <encrypted message>   -->  <name> > <message>
+        didchat <path> write <name> <message>      -->  <encrypted message>
+        didchat <path> read <encrypted message>    -->  <name> <message>
 
-    Example - Send to self:
+    Example - Write to self:
         didchat . init
         didchat . connect self $(didchat . did)
-        didchat . open $(didchat . send self \"Hello self!\")
+        didchat . read $(didchat . write self \"Hello self!\")
 
-    Example - Send to peer:
+    Example - Write to peer:
         didchat jonas init
         didchat snorre init
         
         didchat snorre connect jonas $(didchat jonas did)
         didchat jonas connect snorre $(didchat snorre did)
 
-        didchat jonas open $(didchat snorre send jonas \"Hello Jonas. How are you?\")
-        didchat snorre open $(didchat jonas send snorre \"Hi Snorre:) I have seen better days.\")
+        didchat jonas read $(didchat snorre write jonas \"Hello Jonas. How are you?\")
+        didchat snorre read $(didchat jonas write snorre \"Hi Snorre:) I have seen better days.\")
 "))
 }
 
@@ -202,9 +227,10 @@ enum CMD {
     Init{ path: String },
     Doc{ path: String },
     Did{ path: String },
+    Messages{ path: String },
     Connect{ path: String, name: String, did: String },
-    Send{ path: String, name: String, message: String },
-    Open{ path: String, encrypted_message: String },
+    Write{ path: String, name: String, message: String },
+    Read{ path: String, encrypted_message: String },
     Help
 }
 
@@ -214,21 +240,17 @@ pub struct Config {
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, std::io::Error> {
-        let valid_cmds = vec!["help", "did", "doc", "init", "connect", "send", "open"];
         let default_cmd = String::from("help");
         
-        let path = args.get(1).unwrap_or(&default_cmd);
-        let cmd = args.get(2).unwrap_or(&default_cmd);
+        let path = args.get(1).unwrap_or(&default_cmd).clone();
+        let cmd = args.get(2).unwrap_or(&default_cmd).clone();
 
         let cmd = if args.len() < 3 {
+            eprintln!("Command missing!");
             default_cmd.clone()
-        } else if valid_cmds.contains(&&cmd[..]) {
+        } else {
             cmd.clone()
-        } else  {
-            default_cmd.clone()
         };
-
-        let path = path.clone();
 
         let cmd: CMD = match &cmd[..] {
             "did" => {
@@ -239,6 +261,9 @@ impl Config {
             },
             "init" => {
                 CMD::Init{ path }
+            },
+            "messages" => {
+                CMD::Messages{ path }
             },
             "connect" => {
                 let name = (match args.get(3) {
@@ -253,7 +278,7 @@ impl Config {
                 
                 CMD::Connect{ path, name, did }
             },
-            "send" => {
+            "write" => {
                 let name = (match args.get(3) {
                     Some(arg) => arg,
                     None => return Ok(Config{ cmd: CMD::Help }),
@@ -264,18 +289,21 @@ impl Config {
                     None => return Ok(Config{ cmd: CMD::Help }),
                 }).clone();
 
-                CMD::Send{ path, name, message }
+                CMD::Write{ path, name, message }
             },
-            "open" => {
+            "read" => {
                 let encrypted_message = (match args.get(3) {
                     Some(arg) => arg,
                     None => return Ok(Config{ cmd: CMD::Help }),
                 }).clone();
 
-                CMD::Open{ path, encrypted_message }
+                CMD::Read{ path, encrypted_message }
             },
             "help" => CMD::Help,
-            &_ => CMD::Help,
+            &_ => {
+                eprintln!("{} not a valid command!", cmd);
+                CMD::Help
+            },
         };
 
         Ok(Config { cmd })
@@ -319,3 +347,53 @@ fn did_path(path: &str, did: &str) -> String {
     format!("{}/.didchat/dids/{}", path, did)
 }
 
+fn encrypt_didcomm(from_key: &did_key::Ed25519KeyPair, to_key: &did_key::Ed25519KeyPair, message: &str) -> Result<String, didcomm_rs::Error> {
+    use did_key::Ecdh;
+    use did_key::DIDCore;
+
+    // 1. Get dids
+    let from_did = from_key.get_did_document(did_key::CONFIG_LD_PUBLIC).id;
+    let to_did = to_key.get_did_document(did_key::CONFIG_LD_PUBLIC).id;
+
+    // 2. Map Ed25519 -> x25519
+    let from_key = from_key.get_x25519();
+    let to_key = to_key.get_x25519();
+
+    // 3. Make shared secret (from -> to)
+    let shared_secret = from_key.key_exchange(&to_key);
+
+    // 4. Make didcomm message
+    let to_vec = vec![&to_did[..]];
+    
+    let message = didcomm_rs::Message::new()
+        .from(&from_did)
+        .to(&to_vec[..])
+        .timed(Some(3600))
+        .body(message.as_bytes())
+        .as_jwe(&didcomm_rs::crypto::CryptoAlgorithm::XC20P);
+
+    // 5. Seal message using shared secret
+    let encrypted_message = message
+        .seal(&shared_secret)
+        .unwrap();
+
+    Ok(encrypted_message)
+}
+
+fn decrypt_didcomm(from_key: &did_key::Ed25519KeyPair, to_key: &did_key::Ed25519KeyPair, encrypted_message: &str)-> Result<String, didcomm_rs::Error> {
+    use did_key::Ecdh;
+
+    // 1. Map Ed25519 -> x25519
+    let to_key = to_key.get_x25519();
+    let from_key = from_key.get_x25519();
+
+    // 2. Make shared secret (to -> from)
+    let shared_secret = to_key.key_exchange(&from_key);
+
+    // 3. Decrypt message
+    let decrypted = didcomm_rs::Message::receive(encrypted_message, Some(&shared_secret), None);
+    let decrypted = decrypted.unwrap();
+    let decrypted = String::from_utf8(decrypted.body).unwrap();
+
+    Ok(decrypted)
+}
